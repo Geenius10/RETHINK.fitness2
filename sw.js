c8bdc7ae19cebbf4062082bf1a38fa1c10029c25
Geenius10 <janngeorge9@gmail.com>
2026-09-14 21:30:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='rethink-stundenplan-v2';
+const FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./logo.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
