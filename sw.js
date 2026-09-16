febcd996062b1769b0268528636c81e4aab27c81
Geenius10 <janngeorge9@gmail.com>
2026-09-16 22:55:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='rethink-stundenplan-v2';
-const FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./logo.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='rethink-fitness-20260916-v75-logic';
+const ASSETS=[
+ './', './index.html','./app.css','./app-core.js','./runtime-current.js','./foods.js','./manifest.webmanifest',
+ './logo.png','./icon-180.png','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon-32.png','./favicon-64.png'
+];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return response}).catch(()=>caches.match(event.request).then(hit=>hit||(event.request.mode==='navigate'?caches.match('./index.html'):Response.error()))))
+});
